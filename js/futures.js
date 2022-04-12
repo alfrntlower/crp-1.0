0f008a064a73315598c10b4b94c57a8e2a83ce7f
Aleksey Smychenko <93550750+alfrntlower@users.noreply.github.com>
2022-04-12 21:47:22 +0300

    Add Futures

---
+++ js/futures.js
@@ -0,0 +1,75 @@
+
+const calcBtn = document.querySelector(".js-btn");
+
+const bet = document.querySelector('[name=bet]');
+
+const long = document.querySelector('[name=long]');
+const short = document.querySelector('[name=short]');
+
+const price = document.querySelector('[name=price]');
+const tp = document.querySelector('[name=takeprofit]');
+
+const profit = document.querySelector(".js-profit");
+
+let betValue = 0;
+let priceValue = 0;
+let tpValue = 0;
+
+bet.addEventListener("change", () => {
+    if (bet.value.includes(",")) {
+        alert("Please use DOT instead of COMMA");
+    }
+})
+
+price.addEventListener("change", () => {
+    if (price.value.includes(",")) {
+        alert("Please use DOT instead of COMMA");
+    }
+})
+
+tp.addEventListener("change", () => {
+    if (tp.value.includes(",")) {
+        alert("Please use DOT instead of COMMA");
+    }
+})
+
+function onCalcBtnClick() {
+
+    if (long.checked) {
+        calculateLong();
+    }
+
+    if (short.checked) {
+        calculateShort();
+    }
+
+    else if (long.checked || short.checked) {
+        return alert("Please select LONG or SHORT");
+        
+    }
+
+}
+
+calcBtn.addEventListener("click", onCalcBtnClick);
+
+function calculateLong() {
+
+    betValue = Number(bet.value);
+    priceValue = Number(price.value);
+    tpValue = Number(tp.value);
+
+    const profitValue = ((1/priceValue-1/tpValue)*betValue)*tpValue;
+    profit.textContent = ` ${Number(profitValue).toFixed(2)} $$$`;
+
+}
+
+function calculateShort() {
+
+    betValue = Number(bet.value);
+    priceValue = Number(price.value);
+    tpValue = Number(tp.value);
+
+    const profitValue = (1/priceValue-1/tpValue)*(-betValue)*tpValue;
+    profit.textContent = ` ${Number(profitValue).toFixed(2)} $$$`
+
+}
